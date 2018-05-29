ff283b4033e99ea55d58361a7c0804f27a2612dd
Joffrey Demetz <joffrey.demetz@gmail.com>
2018-05-29 23:47:38 +0200

    2.0.1

--- Tests/RegistryTest.php
+++ Tests/RegistryTest.php
@@ -1,15 +1,14 @@
 <?php
 /**
  * Joffrey Demetz <joffrey.demetz@gmail.com>
- * <http://joffreydemetz.com>
+ * <https://joffrey.demetz.com>
  */
 namespace JDZ\Registry;
 
 use PHPUnit\Framework\TestCase;
 
 /**
- * @package Test
- * @author  Joffrey Demetz <joffrey.demetz@gmail.com>
+ * @author Joffrey Demetz <joffrey.demetz@gmail.com>
  */
 class RegistryTest extends TestCase
 {
